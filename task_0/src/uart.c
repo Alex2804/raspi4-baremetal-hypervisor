452eef4108410d697bbc9584920b01ea19711f33
Alexander Muth <alexander.muth@stud.tu-darmstadt.de>
2024-02-18 17:11:58 +0100

    Updated task_0 for real hardware.

--- task_0/src/uart.c
+++ task_0/src/uart.c
@@ -1,103 +1,200 @@
-/*
- * Copyright (C) 2018 bzt (bztsrc@github)
- *
- * Permission is hereby granted, free of charge, to any person
- * obtaining a copy of this software and associated documentation
- * files (the "Software"), to deal in the Software without
- * restriction, including without limitation the rights to use, copy,
- * modify, merge, publish, distribute, sublicense, and/or sell copies
- * of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be
- * included in all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
- * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
- * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
- * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
- * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
- * DEALINGS IN THE SOFTWARE.
- *
- */
+#include "../../config.h"
 
-#include "gpio.h"
+#if BUILD_FOR_QEMU
+#   define MMIO_BASE       0x3F000000
+#else
+#   define MMIO_BASE       0xFE000000
+#endif
 
-/* Auxilary mini UART registers */
-#define AUX_ENABLE      ((volatile unsigned int*)(MMIO_BASE+0x00215004))
-#define AUX_MU_IO       ((volatile unsigned int*)(MMIO_BASE+0x00215040))
-#define AUX_MU_IER      ((volatile unsigned int*)(MMIO_BASE+0x00215044))
-#define AUX_MU_IIR      ((volatile unsigned int*)(MMIO_BASE+0x00215048))
-#define AUX_MU_LCR      ((volatile unsigned int*)(MMIO_BASE+0x0021504C))
-#define AUX_MU_MCR      ((volatile unsigned int*)(MMIO_BASE+0x00215050))
-#define AUX_MU_LSR      ((volatile unsigned int*)(MMIO_BASE+0x00215054))
-#define AUX_MU_MSR      ((volatile unsigned int*)(MMIO_BASE+0x00215058))
-#define AUX_MU_SCRATCH  ((volatile unsigned int*)(MMIO_BASE+0x0021505C))
-#define AUX_MU_CNTL     ((volatile unsigned int*)(MMIO_BASE+0x00215060))
-#define AUX_MU_STAT     ((volatile unsigned int*)(MMIO_BASE+0x00215064))
-#define AUX_MU_BAUD     ((volatile unsigned int*)(MMIO_BASE+0x00215068))
+// GPIO
 
-/**
- * Set baud rate and characteristics (115200 8N1) and map to GPIO
- */
-void uart_init()
+enum {
+    PERIPHERAL_BASE = MMIO_BASE,
+    GPFSEL0         = PERIPHERAL_BASE + 0x200000,
+    GPSET0          = PERIPHERAL_BASE + 0x20001C,
+    GPCLR0          = PERIPHERAL_BASE + 0x200028,
+    GPPUPPDN0       = PERIPHERAL_BASE + 0x2000E4
+};
+
+enum {
+    GPIO_MAX_PIN       = 53,
+    GPIO_FUNCTION_ALT5 = 2,
+};
+
+enum {
+    Pull_None = 0,
+};
+
+void mmio_write(long reg, unsigned int val) { *(volatile unsigned int *)reg = val; }
+unsigned int mmio_read(long reg) { return *(volatile unsigned int *)reg; }
+
+unsigned int gpio_call(unsigned int pin_number, unsigned int value, unsigned int base, unsigned int field_size, unsigned int field_max) {
+    unsigned int field_mask = (1 << field_size) - 1;
+
+    if (pin_number > field_max) return 0;
+    if (value > field_mask) return 0;
+
+    unsigned int num_fields = 32 / field_size;
+    unsigned int reg = base + ((pin_number / num_fields) * 4);
+    unsigned int shift = (pin_number % num_fields) * field_size;
+
+    unsigned int curval = mmio_read(reg);
+    curval &= ~(field_mask << shift);
+    curval |= value << shift;
+    mmio_write(reg, curval);
+
+    return 1;
+}
+
+unsigned int gpio_set     (unsigned int pin_number, unsigned int value) { return gpio_call(pin_number, value, GPSET0, 1, GPIO_MAX_PIN); }
+unsigned int gpio_clear   (unsigned int pin_number, unsigned int value) { return gpio_call(pin_number, value, GPCLR0, 1, GPIO_MAX_PIN); }
+unsigned int gpio_pull    (unsigned int pin_number, unsigned int value) { return gpio_call(pin_number, value, GPPUPPDN0, 2, GPIO_MAX_PIN); }
+unsigned int gpio_function(unsigned int pin_number, unsigned int value) { return gpio_call(pin_number, value, GPFSEL0, 3, GPIO_MAX_PIN); }
+
+void gpio_use_as_alt_5(unsigned int pin_number) {
+    gpio_pull(pin_number, Pull_None);
+    gpio_function(pin_number, GPIO_FUNCTION_ALT5);
+}
+
+// UART
+
+enum {
+    AUX_BASE        = PERIPHERAL_BASE + 0x215000,
+    AUX_ENABLES     = AUX_BASE + 4,
+    AUX_MU_IO_REG   = AUX_BASE + 64,
+    AUX_MU_IER_REG  = AUX_BASE + 68,
+    AUX_MU_IIR_REG  = AUX_BASE + 72,
+    AUX_MU_LCR_REG  = AUX_BASE + 76,
+    AUX_MU_MCR_REG  = AUX_BASE + 80,
+    AUX_MU_LSR_REG  = AUX_BASE + 84,
+    AUX_MU_CNTL_REG = AUX_BASE + 96,
+    AUX_MU_BAUD_REG = AUX_BASE + 104,
+    AUX_UART_CLOCK  = 500000000,
+    UART_MAX_QUEUE  = 16 * 1024
+};
+
+#define AUX_MU_BAUD(baud) ((AUX_UART_CLOCK/(baud*8))-1)
+
+
+unsigned int uart_is_write_byte_ready() {
+    return mmio_read(AUX_MU_LSR_REG) & 0x20;
+}
+void uart_write_byte_blocking(unsigned char c) {
+    while(!uart_is_write_byte_ready()) {}
+    mmio_write(AUX_MU_IO_REG, c);
+}
+
+
+void uart_init() {
+    mmio_write(AUX_ENABLES, 1); //enable UART1
+    mmio_write(AUX_MU_IER_REG, 0);
+    mmio_write(AUX_MU_CNTL_REG, 0);
+    mmio_write(AUX_MU_LCR_REG, 3); //8 bits
+    mmio_write(AUX_MU_MCR_REG, 0);
+    mmio_write(AUX_MU_IER_REG, 0);
+    mmio_write(AUX_MU_IIR_REG, 0xC6); //disable interrupts
+    mmio_write(AUX_MU_BAUD_REG, AUX_MU_BAUD(115200));
+    gpio_use_as_alt_5(14);
+    gpio_use_as_alt_5(15);
+    mmio_write(AUX_MU_CNTL_REG, 3); //enable RX/TX
+}
+
+
+char uart_try_read_char() {
+    if(mmio_read(AUX_MU_LSR_REG) & 0x01) {  // check if char in buffer
+        char r = mmio_read(AUX_MU_IO_REG);  // read char in buffer
+        return r == '\r' ? '\n' : r;
+    }
+    return '\0';  // else return null terminator
+}
+char uart_read_char() {
+    do {
+        asm volatile("nop");
+    } while(!(mmio_read(AUX_MU_LSR_REG) & 0x01));
+    char r = mmio_read(AUX_MU_IO_REG);  // read char in buffer
+    return r == '\r' ? '\n' : r;
+}
+
+
+void uart_write_newline() {
+    uart_write_byte_blocking('\r');
+    uart_write_byte_blocking('\n');
+}
+void uart_write_byte(unsigned char c) {
+    uart_write_byte_blocking(c);
+}
+void uart_write_char(char c) {
+    uart_write_byte_blocking(c);
+}
+void uart_write_string(char *buffer) {
+    while(*buffer) {
+        if(*buffer == '\n') {
+            uart_write_byte_blocking('\r');
+        }
+        uart_write_byte_blocking(*buffer++);
+    }
+}
+void uart_write_hex(unsigned long long h) {
+    uart_write_byte_blocking('0');
+    uart_write_byte_blocking('x');
+    int flag = 0;
+    for(int c = 60; c >= 0; c -= 4) {
+        unsigned long long n = (h>>c) & 0xF; // get highest tetrad
+        if(n != 0 || flag != 0 || c == 0) {
+            flag = 1;
+            n += n > 9 ? 0x37 : 0x30; // 0-9 => '0'-'9', 10-15 => 'A'-'F'
+            uart_write_byte_blocking(n);
+        }
+    }
+}
+void uart_write_long(long long d) {
+    if(d < 0) {
+        uart_write_byte_blocking('-');
+        d = -d;
+    }
+
+    long flag = 0;
+    for(long long c = 1000000000000000000; c >= 1; c /= 10) {
+        // get highest digit
+        long long n = (d/c) % 10;
+        if(n != 0 || flag != 0 || c == 1){
+            flag = 1;
+            // 0-9 => '0'-'9'
+            n += 0x30;
+            uart_write_byte_blocking(n);
+        }
+    }
+}
+
+
+void uart_dump(void *ptr)
 {
-    register unsigned int r;
-
-    /* initialize UART */
-    *AUX_ENABLE |=1;       // enable UART1, AUX mini uart
-    *AUX_MU_CNTL = 0;
-    *AUX_MU_LCR = 3;       // 8 bits
-    *AUX_MU_MCR = 0;
-    *AUX_MU_IER = 0;
-    *AUX_MU_IIR = 0xc6;    // disable interrupts
-    *AUX_MU_BAUD = 270;    // 115200 baud
-    /* map UART1 to GPIO pins */
-    r=*GPFSEL1;
-    r&=~((7<<12)|(7<<15)); // gpio14, gpio15
-    r|=(2<<12)|(2<<15);    // alt5
-    *GPFSEL1 = r;
-    *GPPUD = 0;            // enable pins 14 and 15
-    r=150; while(r--) { asm volatile("nop"); }
-    *GPPUDCLK0 = (1<<14)|(1<<15);
-    r=150; while(r--) { asm volatile("nop"); }
-    *GPPUDCLK0 = 0;        // flush GPIO setup
-    *AUX_MU_CNTL = 3;      // enable Tx, Rx
-}
-
-/**
- * Send a character
- */
-void uart_send(unsigned int c) {
-    /* wait until we can send */
-    do{asm volatile("nop");}while(!(*AUX_MU_LSR&0x20));
-    /* write the character to the buffer */
-    *AUX_MU_IO=c;
-}
-
-/**
- * Receive a character
- */
-char uart_getc() {
-    char r;
-    /* wait until something is in the buffer */
-    do{asm volatile("nop");}while(!(*AUX_MU_LSR&0x01));
-    /* read it and return */
-    r=(char)(*AUX_MU_IO);
-    /* convert carriage return to newline */
-    return r=='\r'?'\n':r;
-}
-
-/**
- * Display a string
- */
-void uart_puts(char *s) {
-    while(*s) {
-        /* convert newline to carriage return + newline */
-        if(*s=='\n')
-            uart_send('\r');
-        uart_send(*s++);
+    unsigned long a, b, d;
+    unsigned char c;
+    for(a = (unsigned long)ptr; a < (unsigned long)ptr+512; a += 16) {
+        uart_write_hex(a);
+        uart_write_byte_blocking(':');
+        uart_write_byte_blocking(' ');
+        for(b = 0; b < 16; ++b) {
+            c = *(unsigned char*)(a+b);
+            d = (unsigned int)c;
+            d >>= 4;
+            d &= 0xF;
+            d += d > 9 ? 0x37 : 0x30;
+            uart_write_byte_blocking(d);
+            d = (unsigned int)c;
+            d &= 0xF;
+            d += d > 9 ? 0x37 : 0x30;
+            uart_write_byte_blocking(d);
+            uart_write_byte_blocking(' ');
+            if(b % 4 == 3)
+                uart_write_byte_blocking(' ');
+        }
+        for(b=0;b<16;b++) {
+            c = *(unsigned char*)(a+b);
+            uart_write_byte_blocking(c < 32 || c >= 127 ? '.' : c);
+        }
+        uart_write_byte_blocking('\r');
+        uart_write_byte_blocking('\n');
     }
 }
